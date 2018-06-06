18fd950973ea829fd27d19c7ab2d73ceca51d174
Kaelinator <kirkkael@gmail.com>
2018-06-05 22:59:36 -0500

    it sorta worked!!!

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
+const OAuth = require('oauth').OAuth
+const session = require('express-session')
 
 dotenv.config()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60000 }
+}))
+
+const oa = new OAuth(
+  'https://www.khanacademy.org/api/auth2/request_token',
+  'https://www.khanacademy.org/api/auth2/access_token',
+  process.env.CONSUMER_KEY,
+  process.env.CONSUMER_SECRET,
+  '1.0',
+  'https://khan-vr-api.herokuapp.com/oauth/callback',
+  'HMAC-SHA1',
+  64
+)
+
+app.get('/oauth', (req, res) => {
+  oa.getOAuthRequestToken(function requestTokenCallback(err, token, token_secret, results) {
+
+    if (err) {
+      console.log('requestTokenCallback', err)
+      return
+    }
+
+    console.log('token:', token)
+    console.log('tokenSecret:', token_secret)
+    req.session.oauth = req.session.oauth || {}
+    req.session.oauth.token = token
+    req.session.oauth.token_secret = token_secret
+    res.redirect('https://www.khanacademy.org/api/auth2/authorize?oauth_token=' + token)
+  })
 })
 
-app.get('/done', (req, res) => {
-  res.send('it worked!')
-})
+app.get('/oauth/callback', (req, res, next) => {
+  if (req.session.oauth) {
+    req.session.oauth.verifier = req.query.oauth_verifier
+    const oauth = req.session.oauth
 
-app.get('/oauth', require('./public/oauth'))
+    oa.getOAuthAccessToken(oauth.token,
+      oauth.token_secret,
+      oauth.verifier,
+      function (error, oauth_access_token, oauth_access_token_secret, results) {
+
+        if (err) {
+          console.log('getOAuthAccessToken', err)
+          return
+        }
+        console.log(results)
+      })
+  } else {
+    next(new Error('How did you get here?'))
+  }
+})
 
 app.listen(process.env.PORT || 3000)
--- public/oauth.js
+++ public/oauth.js
@@ -1,38 +1,13 @@
 
-const OAuth = require('oauth').OAuth
 const util = require('util')
 
-module.exports = () => {
+module.exports = (req, res) => {
 
-  console.log('key:', process.env.CONSUMER_KEY)
-  console.log('secret:', process.env.CONSUMER_SECRET)
+  
 
-  const oauth = new OAuth(
-    'https://www.khanacademy.org/api/auth2/request_token',
-    'https://www.khanacademy.org/api/auth2/access_token',
-    process.env.CONSUMER_KEY,
-    process.env.CONSUMER_SECRET,
-    '1.0',
-    'https://khan-vr-api.herokuapp.com/',
-    'HMAC-SHA1',
-    64
-  )
+  
 
-  function requestTokenCallback(err, token, tokenSecret, res) {
-
-    if (err) {
-      console.log('requestTokenCallback', err)
-      return
-    }
-
-    console.log('token:', token)
-    console.log('tokenSecret:', tokenSecret)
-    console.log('requestTokenCallback res:', util.inspect(res))
-    console.log('requesting access token')
-    oauth.getOAuthAccessToken(token, tokenSecret, accessTokenCallback)
-  }
-
-  oauth.getOAuthRequestToken(requestTokenCallback)
+  
   
   function accessTokenCallback(err, accessToken, accessTokenSecret, res) {
 
