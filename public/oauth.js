62b2ee86c3ccea59a93e008c7ad5c1383545fd12
Kaelinator <kirkkael@gmail.com>
2018-06-05 21:35:51 -0500

    oauth doesn't work

--- public/oauth.js
+++ public/oauth.js
@@ -1,61 +1,67 @@
 
 const OAuth = require('oauth').OAuth
+const util = require('util')
 
 module.exports = () => {
+
+  console.log('key:', process.env.CONSUMER_KEY)
+  console.log('secret:', process.env.CONSUMER_SECRET)
+
   const oauth = new OAuth(
     'https://www.khanacademy.org/api/auth2/request_token',
     'https://www.khanacademy.org/api/auth2/access_token',
     process.env.CONSUMER_KEY,
     process.env.CONSUMER_SECRET,
     '1.0',
-    'https://khan-vr-api.herokuapp.com/done',
+    'https://khan-vr-api.herokuapp.com/',
     'HMAC-SHA1',
     64
   )
 
+  function requestTokenCallback(err, token, tokenSecret, res) {
+
+    if (err) {
+      console.log('requestTokenCallback', err)
+      return
+    }
+
+    console.log('token:', token)
+    console.log('tokenSecret:', tokenSecret)
+    console.log('requestTokenCallback res:', util.inspect(res))
+    console.log('requesting access token')
+    oauth.getOAuthAccessToken(token, tokenSecret, accessTokenCallback)
+  }
+
   oauth.getOAuthRequestToken(requestTokenCallback)
-}
+  
+  function accessTokenCallback(err, accessToken, accessTokenSecret, res) {
 
-const finalCallback = (err, data, res) => {
+    if (err) {
+      console.log('accessTokenCallback', err)
+      return
+    }
 
-  if (err) {
-    console.log('finalCallback', err)
-    return
+    console.log('accessToken:', accessToken)
+    console.log('accessTokenSecret:', accessTokenSecret)
+    console.log('accessTokenCallback res:', res)
+    oauth.getProtectedResource(
+      'https://www.khanacademy.org/api/v1/user',
+      'GET',
+      accessToken,
+      accessTokenSecret,
+      finalCallback
+    )
   }
 
-  console.log('finalCallback data:', data)
-  console.log('finalCallback res:', res)
-}
+  function finalCallback(err, data, res) {
 
-const accessTokenCallback = (err, accessToken, accessTokenSecret, res) => {
+    if (err) {
+      console.log('finalCallback', err)
+      return
+    }
 
-  if (err) {
-    console.log('accessTokenCallback', err)
-    return
+    console.log('finalCallback data:', data)
+    console.log('finalCallback res:', res)
   }
-
-  console.log('accessToken:', accessToken)
-  console.log('accessTokenSecret:', accessTokenSecret)
-  console.log('accessTokenCallback res:', res)
-  oauth.getProtectedResource(
-    'https://www.khanacademy.org/api/v1/user',
-    'GET',
-    accessToken,
-    accessTokenSecret,
-    finalCallback
-  )
 }
 
-const requestTokenCallback = (err, token, tokenSecret, res) => {
-
-  if (err) {
-    console.log('requestTokenCallback', err)
-    return
-  }
-
-  console.log('token:', token)
-  console.log('tokenSecret:', tokenSecret)
-  console.log('requestTokenCallback res:', res)
-  console.log('requesting access token')
-  oauth.getOAuthAccessToken(token, tokenSecret, accessTokenCallback)
-}
