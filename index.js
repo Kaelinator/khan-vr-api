1f2845ea8858526d9cbbae8194788a4954ec9836
Kaelinator <kirkkael@gmail.com>
2018-06-06 21:29:54 -0500

    don't even need verifier

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const app = express()
 const dotenv = require('dotenv')
 const OAuth = require('oauth').OAuth
 const session = require('express-session')
-const khan = require('khan')(process.env.CONSUMER_KEY, process.env.CONSUMER_SECRET)
-
 dotenv.config()
+const khan = require('khan')(process.env.CONSUMER_KEY, process.env.CONSUMER_SECRET)
 
 app.use(session({
   secret: 'keyboard cat',
@@ -14,59 +13,32 @@ app.use(session({
   cookie: { maxAge: 60000 }
 }))
 
-const oa = new OAuth(
-  'https://www.khanacademy.org/api/auth2/request_token',
-  'https://www.khanacademy.org/api/auth2/access_token',
-  process.env.CONSUMER_KEY,
-  process.env.CONSUMER_SECRET,
-  '1.0',
-  'http://localhost:3000/oauth/callback',
-  'HMAC-SHA1',
-  64
-)
 
 app.get('/oauth', (req, res) => {
-  oa.getOAuthRequestToken(function requestTokenCallback(err, token, token_secret, results) {
 
-    if (err) {
-      console.log('requestTokenCallback', err)
-      return
-    }
+  khan
+    .requestToken()
+    .then(tokens => {
 
-    req.session.oauth = req.session.oauth || {}
-    req.session.oauth.token = token
-    req.session.oauth.token_secret = token_secret
-    res.redirect(`https://www.khanacademy.org/api/auth2/authorize?oauth_token=${token}`)
-  })
+      const authorize = 'https://www.khanacademy.org/api/auth2/authorize'
+      // const callback = encodeURIComponent('http://localhost:3000/oauth/callback')
+      console.log(tokens)
+      const callback = 'http://localhost:3000/oauth/callback'
+      const str = `${authorize}?oauth_token=${tokens['oauth_token']}&oauth_callback=${callback}`
+      console.log(str)
+      res.redirect(str)
+    })
 })
 
 app.get('/oauth/callback', (req, res, next) => {
-  console.log('session:', req.session.oauth)
-  if (req.session.oauth) {
-    // req.session.oauth.verifier = req.query.oauth_verifier
-    // const oauth = req.session.oauth
 
-    oa.getOAuthAccessToken(req.session.oauth.token,
-      req.session.oauth.token_secret,
-      req.query.oauth_verifier,
-      function (error, oauth_access_token_secret, oauth_access_token, results) {
+  console.log('oauth_token:', req.query['oauth_token'])
+  console.log('oauth_token_secret:', req.query['oauth_token_secret'])
 
-        if (error) {
-          console.log('getOAuthAccessToken', error)
-          return
-        }
-        console.log(results)
-
-        khan(oauth_access_token, oauth_access_token_secret)
-          .user()
-          .then(u => res.json({ u }))
-          .catch(err => res.json({ err, oauth_access_token, oauth_access_token_secret }))
-        // .accessToken(req.session.oauth.token, req.query.oauth_verifier)
-
-      })
-  } else {
-    res.send('How\'d you get here?')
-  }
+  khan.accessToken(req.query['oauth_token'], undefined, req.query['oauth_token_secret'])
+    .then(({ oauth_token, oauth_token_secret }) => khan(oauth_token_secret, oauth_token).user())
+    .then(u => res.json({ u }))
+    .catch(err => res.json({ err }))
 })
 
 app.listen(process.env.PORT || 3000)
