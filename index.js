9d8d8b0b57baf6beace590aa1d1b5e15e134c321
Kaelinator <kirkkael@gmail.com>
2018-06-06 21:50:15 -0500

    it works!

--- index.js
+++ index.js
@@ -1,41 +1,21 @@
 const express = require('express')
 const app = express()
-const dotenv = require('dotenv')
-const OAuth = require('oauth').OAuth
-const session = require('express-session')
-dotenv.config()
+require('dotenv').config()
 const khan = require('khan')(process.env.CONSUMER_KEY, process.env.CONSUMER_SECRET)
-
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { maxAge: 60000 }
-}))
-
 
 app.get('/oauth', (req, res) => {
 
   khan
-    .requestToken()
+    .requestToken('http://localhost:3000/oauth/callback')
     .then(tokens => {
 
-      const authorize = 'https://www.khanacademy.org/api/auth2/authorize'
-      // const callback = encodeURIComponent('http://localhost:3000/oauth/callback')
-      console.log(tokens)
-      const callback = 'http://localhost:3000/oauth/callback'
-      const str = `${authorize}?oauth_token=${tokens['oauth_token']}&oauth_callback=${callback}`
-      console.log(str)
-      res.redirect(str)
+      res.redirect(`https://www.khanacademy.org/api/auth2/authorize?oauth_token=${tokens['oauth_token']}`)
     })
 })
 
 app.get('/oauth/callback', (req, res, next) => {
 
-  console.log('oauth_token:', req.query['oauth_token'])
-  console.log('oauth_token_secret:', req.query['oauth_token_secret'])
-
-  khan.accessToken(req.query['oauth_token'], undefined, req.query['oauth_token_secret'])
+  khan.accessToken(req.query['oauth_token'], req.query['oauth_verifier'], req.query['oauth_token_secret'])
     .then(({ oauth_token, oauth_token_secret }) => khan(oauth_token_secret, oauth_token).user())
     .then(u => res.json({ u }))
     .catch(err => res.json({ err }))
